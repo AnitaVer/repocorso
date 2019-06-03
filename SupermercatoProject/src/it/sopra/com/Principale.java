3501dc48bdf74200cba8a59c2fcea69330464b99
AnitaVer <anitaverrilli@gmail.com>
2019-06-03 20:24:18 +0200

    cliente B

---
+++ SupermercatoProject/src/it/sopra/com/Principale.java
@@ -0,0 +1,12 @@
+package it.sopra.com;
+
+public class Principale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cliente clienteB = new Cliente("codiceB", "Fra", "Verrilli", 100);
+		Prodotto prodotto1b = new Prodotto("prodotto1b", "prodotto1b", 50.00, 3);
+	}
+
+}
