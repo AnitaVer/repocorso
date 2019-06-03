6529f8eacc4ef099cb007098c35025f927ce996a
AnitaVerrilli <anitaverrilli@hotmail.it>
2019-06-03 18:48:31 +0200

    persona francesca

--- IndirizzoProject/src/it/sopra/com/Client.java
+++ IndirizzoProject/src/it/sopra/com/Client.java
@@ -1,10 +1,13 @@
 package it.sopra.com;
 
+import java.util.Date;
+
 public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Persona francesca = new Persona("Francesca", "Verrilli", new Date("19/08/1988"), new Indirizzo("Croci", 2, "Cf", "82022", "BN", "Campania"));
+		System.out.println(francesca.toString());
 	}
 
 }
