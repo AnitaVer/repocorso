326bb2fe8a89da70ea30d7204b0a57c1e9c453f2
AnitaVerrilli <anitaverrilli@hotmail.it>
2019-06-03 20:27:19 +0200

    cliente a

---
+++ SupermercatoProject/src/it/sopra/com/Principale.java
@@ -0,0 +1,14 @@
+package it.sopra.com;
+
+public class Principale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cliente clienteA = new Cliente("codiceA", "Anita", "Verrilli", 10);
+		Prodotto prodotto1a = new Prodotto("prodotto1a", "prodotto1a", 10.00, 2);
+		Prodotto prodotto1b = new Prodotto("prodotto1b", "prodotto1b", 20.00, 5);
+
+	}
+
+}
