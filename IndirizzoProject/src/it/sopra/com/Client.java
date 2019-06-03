1e4ba242c32725085e8e921173b658e0888220b1
AnitaVer <anitaverrilli@gmail.com>
2019-06-03 18:46:20 +0200

    persona anita

--- IndirizzoProject/src/it/sopra/com/Client.java
+++ IndirizzoProject/src/it/sopra/com/Client.java
@@ -1,10 +1,13 @@
 package it.sopra.com;
 
+import java.util.Date;
+
 public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Persona anita = new Persona("anita", "verrilli", new Date("20/02/1990"), new Indirizzo("croci", 4, "castelf", "82022", "Benevento", "Campania"));
+		System.out.println(anita.toString());
 	}
 
 }
